c0b25dbf8c15c8d27a2fbd5c3b75bdd47f25052a
James <lim.en.hui.james@gmail.com>
2020-09-03 10:39:45 +0000

    GridLocking player movement

--- src/scripts/BaseScene.js
+++ src/scripts/BaseScene.js
@@ -5,10 +5,9 @@ import IMGPlayerSprite from '../assets/imgs/player.png';
 
 /** @typedef {import('../Game').default} Game */
 
-// NOTE: TILEMOVEMENT is x * 16 where x must be a multiple of 16,
-// whereas TILETIMESTAMP = 1000 / x
+// NOTE: TILEMOVEMENT is x * 16 where x must be a multiple of 16
 const TILEMOVEMENT = 128;
-const TILETIMESTEP = 125;
+const TILESIZE = 16;
 
 /**
  * BaseScene represents the basic level scene for this RPG game
@@ -22,8 +21,8 @@ export default class BaseScene extends Phaser.Scene {
     super({ key });
     this.instance = { // NOTE: don't want to affect phaser
       game: game,
-      movementLastTimestamp: -1,
-      lastKey: '',
+      gridLock: false,
+      movementFunction: null,
       cursors: null,
       player: null
     };
@@ -49,7 +48,10 @@ export default class BaseScene extends Phaser.Scene {
     /** @type {Phaser.Physics.Arcade.Sprite} */
     const player = this.instance.player;
 
-    this.updateKeyboard(time, cursors, player);
+    this.updateKeyboard(time, delta, cursors, player);
+    if (this.instance.movementFunction) {
+      this.instance.movementFunction();
+    }
   }
 
   /* Own functions */
@@ -69,28 +71,28 @@ export default class BaseScene extends Phaser.Scene {
     this.anims.create({
       key: 'player_up',
       frames: this.anims.generateFrameNumbers('player', { start: 104, end: 112 }),
-      frameRate: 10,
+      frameRate: 13,
       repeat: -1
     });
 
     this.anims.create({
       key: 'player_left',
       frames: this.anims.generateFrameNumbers('player', { start: 117, end: 125 }),
-      frameRate: 10,
+      frameRate: 13,
       repeat: -1
     });
 
     this.anims.create({
       key: 'player_down',
       frames: this.anims.generateFrameNumbers('player', { start: 130, end: 138 }),
-      frameRate: 10,
+      frameRate: 13,
       repeat: -1
     });
 
     this.anims.create({
       key: 'player_right',
       frames: this.anims.generateFrameNumbers('player', { start: 143, end: 151 }),
-      frameRate: 10,
+      frameRate: 13,
       repeat: -1
     });
   }
@@ -104,61 +106,92 @@ export default class BaseScene extends Phaser.Scene {
   }
 
   /**
-   * Swaps out the last key press and tile-locks the player
-   * @private
-   * @param {string} key The key ('w', 'a', 's', 'd')
-   * @param {Phaser.Physics.Arcade.Sprite} player The Arcade sprite that represents the player
-   */
-  changeLastKey (key, player) {
-    if (key === this.instance.lastKey) {
-      return;
-    }
-
-    // NOTE: tiled based RPG, so we'll teleport the player to integer coordinates
-    this.instance.lastKey = key;
-    player.setPosition(Math.round(player.x / 16.0) * 16.0, Math.round(player.y / 16.0) * 16.0);
-  }
-
-  /**
    * Updates keyboard
    * @private
    * @param {number} time
+   * @param {number} delta
    * @param {Object<string, Phaser.Input.Keyboard.Key>} cursors
    * @param {Phaser.Physics.Arcade.Sprite} player
    */
-  updateKeyboard (time, cursors, player) {
-    const allowMovement = (time - this.instance.movementLastTimestamp >= TILETIMESTEP);
+  updateKeyboard (time, delta, cursors, player) {
+    if (this.instance.gridLock) {
+      return;
+    }
 
-    if (allowMovement) {
-      if (cursors.W.isDown && allowMovement) {
-        player.setVelocity(0, -TILEMOVEMENT);
-        player.anims.play('player_up', true);
-        this.instance.movementLastTimestamp = time;
-        this.changeLastKey('w', player);
-      } else if (cursors.S.isDown && allowMovement) {
-        player.setVelocity(0, TILEMOVEMENT);
-        player.anims.play('player_down', true);
-        this.instance.movementLastTimestamp = time;
-        this.changeLastKey('s', player);
-      } else if (cursors.A.isDown && allowMovement) {
-        player.setVelocity(-TILEMOVEMENT, 0);
-        player.anims.play('player_left', true);
-        this.instance.movementLastTimestamp = time;
-        this.changeLastKey('a', player);
-      } else if (cursors.D.isDown && allowMovement) {
-        player.setVelocity(TILEMOVEMENT, 0);
-        player.anims.play('player_right', true);
-        this.instance.movementLastTimestamp = time;
-        this.changeLastKey('d', player);
-      } else if (allowMovement) {
-        // NOTE: causes the animation to stop in the first frame
-        if (player.anims.isPlaying) {
-          player.anims.restart();
-        }
-        player.anims.stop();
-        player.setVelocity(0, 0);
-        this.changeLastKey('', player);
+    // NOTE: I know player.setVelocity exists, but I want
+    // fine control over the x and y positions (they must be multiples of 16)
+    // without the glitching that comes from teleporting the sprite to the right place
+    if (cursors.W.isDown) {
+      player.anims.play('player_up', true);
+      this.instance.gridLock = true;
+      this.instance.movementFunction = () =>
+        this.updatePosition(player, player.x, player.y - ((delta / 1000) * TILEMOVEMENT));
+    } else if (cursors.S.isDown) {
+      player.anims.play('player_down', true);
+      this.instance.gridLock = true;
+      this.instance.movementFunction = () =>
+        this.updatePosition(player, player.x, player.y + ((delta / 1000) * TILEMOVEMENT));
+    } else if (cursors.A.isDown) {
+      player.anims.play('player_left', true);
+      this.instance.gridLock = true;
+      this.instance.movementFunction = () =>
+        this.updatePosition(player, player.x - ((delta / 1000) * TILEMOVEMENT), player.y);
+    } else if (cursors.D.isDown) {
+      player.anims.play('player_right', true);
+      this.instance.gridLock = true;
+      this.instance.movementFunction = () =>
+        this.updatePosition(player, player.x + ((delta / 1000) * TILEMOVEMENT), player.y);
+    } else {
+      // NOTE: causes the animation to stop in the first frame
+      if (player.anims.isPlaying) {
+        player.anims.restart();
       }
+      player.anims.stop();
+      this.instance.movementFunction = null;
+    }
+  }
+
+  /**
+   * Updates the position of the player based on time and delta. Implements gridlock
+   * @private
+   * @param {Phaser.Physics.Arcade.Sprite} player
+   * @param {number} progressNaturalX What X naturally would be without gridlock
+   * @param {number} progressNaturalY What Y naturally would be without gridlock
+   */
+  updatePosition (player, progressNaturalX, progressNaturalY) {
+    if (!this.instance.gridLock) {
+      return;
+    }
+
+    // ensure crossing tiles are done discretely
+    // crossing tiles will also unlock gridLock for a moment
+    const tileFloorDifferenceX = Math.floor(progressNaturalX / TILESIZE) - Math.floor(player.x / TILESIZE);
+    const tileCeilDifferenceX = Math.ceil(progressNaturalX / TILESIZE) - Math.ceil(player.x / TILESIZE);
+    if (tileFloorDifferenceX >= 1) {
+      player.x = Math.floor(progressNaturalX / TILESIZE) * TILESIZE;
+      this.instance.gridLock = false;
+    }
+
+    if (tileCeilDifferenceX <= -1) {
+      player.x = Math.ceil(progressNaturalX / TILESIZE) * TILESIZE;
+      this.instance.gridLock = false;
+    }
+
+    const tileFloorDifferenceY = Math.floor(progressNaturalY / TILESIZE) - Math.floor(player.y / TILESIZE);
+    const tileCeilDifferenceY = Math.ceil(progressNaturalY / TILESIZE) - Math.ceil(player.y / TILESIZE);
+    if (tileFloorDifferenceY >= 1) {
+      player.y = Math.floor(progressNaturalY / TILESIZE) * TILESIZE;
+      this.instance.gridLock = false;
+    }
+
+    if (tileCeilDifferenceY <= -1) {
+      player.y = Math.ceil(progressNaturalY / TILESIZE) * TILESIZE;
+      this.instance.gridLock = false;
+    }
+
+    if (this.instance.gridLock) {
+      player.x = progressNaturalX;
+      player.y = progressNaturalY;
     }
   }
 }
